71d1f44b8770aeeacf9143c8818b32a4f4282743
Aminatakabba <aminata-kabba@hotmail.com>
2020-08-22 19:39:41 +0100

    adding and removal buttons [erfectly working

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -5,12 +5,13 @@ export class Track extends React.Component {
     constructor(props) {
         super(props);
         this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
     }
 
 
     renderAction() {
         if (this.props.isRemoval) {
-            return <buttton className="Track-actions">-</buttton>
+            return <button className="Track-actions" onClick={this.removeTrack}>-</button>
         } else {
             return <buttton className="Track-action" onClick={this.addTrack}>+</buttton>
         }
@@ -18,6 +19,10 @@ export class Track extends React.Component {
 
     addTrack() {
         this.props.onAdd(this.props.track);
+    }
+
+    removeTrack() {
+        this.props.onRemove(this.props.track)
     }
 
     render() {
