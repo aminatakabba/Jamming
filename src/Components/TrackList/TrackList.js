df70c5af84601f285f37c1d2d2e6d47913f3f647
Aminatakabba <aminata-kabba@hotmail.com>
2020-08-22 17:35:30 +0100

    updated TrackList component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './TraclList.css';
+import './TracklList.css';
 
 class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                {/* <Track /> */}
+                
             </div>
         )
     }
