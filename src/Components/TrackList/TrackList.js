157b23dcacc95c8c488ad3951427c0ed4ab40262
Aminatakabba <aminata-kabba@hotmail.com>
2020-08-22 17:27:17 +0100

    TrackList component added to the directory

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TraclList.css';
+
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                {/* <Track /> */}
+            </div>
+        )
+    }
+}
+
+export default TrackList;
