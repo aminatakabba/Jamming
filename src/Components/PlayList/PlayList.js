c1163f859222e4de17e80598a91761d46b4b55b9
Aminatakabba <aminata-kabba@hotmail.com>
2020-08-22 17:20:22 +0100

    Playlist component added to the directory

---
+++ src/Components/PlayList/PlayList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './PlayList.css';
+
+class PlayList extends React.Component {
+    render() {
+        return (
+            <div className="Playlist">
+                <input defaultvalue={"New Playlist"}/>
+                {/* <TrackList /> */}
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        )
+    }
+}
+export default PlayList;
